fca0ff6c74328da862869cc509b8f648a9023d3b
amabdulr <148323982+amabdulr@users.noreply.github.com>
2025-03-24 15:59:46 +0530

    Update and rename generate_infotypes_gui.sh to generate_infotypes_cli.sh

--- generate_infotypes_cli.sh
+++ generate_infotypes_cli.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-output_file="infotypes_unclean_gui.md"
+output_file="infotypes_unclean_cli.md"
 
 echo "##Information Types and Titling Rules" > "$output_file"
 echo "" >> "$output_file"
 
-for file in concept.md task_gui.md process.md reference.md principle.md
+for file in concept.md task.md process.md reference.md principle.md
 do
     section_title=$(basename "$file" .md | sed 's/.*/\u&/')
     echo "### $section_title" >> "$output_file"
