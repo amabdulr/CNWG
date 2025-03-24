15a7412546b02e962436cc23bfd2f6f7483dde36
amabdulr <148323982+amabdulr@users.noreply.github.com>
2025-03-24 16:04:17 +0530

    Update generate_infotypes_cli.sh

--- generate_infotypes_cli.sh
+++ generate_infotypes_cli.sh
@@ -5,7 +5,7 @@ output_file="infotypes_unclean_cli.md"
 echo "##Information Types and Titling Rules" > "$output_file"
 echo "" >> "$output_file"
 
-for file in concept.md task.md process.md reference.md principle.md
+for file in concept.md task_cli.md process.md reference.md principle.md
 do
     section_title=$(basename "$file" .md | sed 's/.*/\u&/')
     echo "### $section_title" >> "$output_file"
