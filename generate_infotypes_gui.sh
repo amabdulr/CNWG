2b29de999617946322c45576ebd957077e0328d8
amabdulr <148323982+amabdulr@users.noreply.github.com>
2025-03-24 15:50:24 +0530

    Update generate_infotypes_gui.sh

--- generate_infotypes_gui.sh
+++ generate_infotypes_gui.sh
@@ -8,7 +8,7 @@ echo "" >> "$output_file"
 for file in concept.md task_gui.md process.md reference.md principle.md
 do
     section_title=$(basename "$file" .md | sed 's/.*/\u&/')
-    echo "### 📘 $section_title" >> "$output_file"
+    echo "### $section_title" >> "$output_file"
     echo "" >> "$output_file"
     cat "$file" >> "$output_file"
     echo "" >> "$output_file"
